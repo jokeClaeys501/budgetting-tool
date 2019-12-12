da4a1f2bae2989745bb9cd72426eb81c81564b65
Seppe Iven <Seppe@Air-van-Claeys.fritz.box>
2019-12-12 21:12:55 +0100

    First graph

---
+++ Pencil42App.Frontend/src/components/MonthlyIncome.vue
@@ -0,0 +1,45 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+  extends: Line,
+  data: () => ({
+     chartdata: {
+       labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+       datasets: [
+         {
+           label: 'Milestone 1',
+           backgroundColor: '#0eacbd',
+           data: [2, 0, 8, 6, 3]
+         },
+         {label: 'Milestone 2',
+          backgroundColor: '#1F2F63',
+          data : [6, 8, 0, 2, 5]
+         }
+       ]
+     },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+        xAxes: [{
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+    },
+  }),
+
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
--- Pencil42App.Frontend/src/views/Home.vue
+++ Pencil42App.Frontend/src/views/Home.vue
@@ -3,6 +3,7 @@
 <div class="home">
         <h1 align="center">Welcome to Sepski's budgetting tool</h1>
         <b-button>Button</b-button>
+        <MonthlyIncome />
     </div>
 
 
@@ -11,11 +12,16 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import VueResource from 'vue-resource';
- 
+import MonthlyIncome from '../components/MonthlyIncome.vue'
+
     export default Vue.extend({
     components: {
-        
+        MonthlyIncome
     },
+    
     });
+
+
+
 
 </script>
